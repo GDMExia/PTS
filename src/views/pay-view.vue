590e0e866e5b9be8df84b34ba726d99a9721eb0a
gabriel.fang <gabriel.fang@ugoodtech.com>
2019-07-28 15:08:41 +0800

    页面规划

---
+++ src/views/pay-view.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="main">
+        <p>请选择充值金额</p>
+        <div class="selectmode">
+            <div class="button" :class="btn==0?'selected':''" @click="change(0)">
+                <p class="price">100元</p>
+                <p class="detail">得300积分</p>
+            </div>
+            <div class="button" :class="btn==1?'selected':''" @click="change(1)">
+                <p class="price">300元</p>
+                <p class="detail">得800积分</p>
+            </div>
+            <div class="button" :class="btn==2?'selected':''" @click="change(2)">
+                <p class="price">500元</p>
+                <p class="detail">得1500积分</p>
+            </div>
+            <div class="placehold"></div>
+        </div>
+        <div class="more">
+            <p>充值说明</p>
+            <ul>
+                <li>1积分=1元</li>
+                <li>积分充值成功后无法退款，不可提现</li>
+                <li>如存在无法充值、充值失败等问题，可在【我的】-【联系客服】中进行咨询</li>
+                <li>积分可在活动支付时按该活动要求进行抵扣</li>
+            </ul>
+        </div>
+        <div class="handle">
+            <div class="button">立即支付</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            btn:0
+        }
+    },
+    methods:{
+        change(val){
+            this.btn=val
+        }
+    }
+}
+</script>
+
+<style scoped>
+.main{width: 100%;background-color: #F8F8F8}
+p{font-size: 16px;color: #222;display: inline-block;margin: 12px 0 0 4%;font-weight: bold}
+.selectmode{width: 100%;display: flex;flex-wrap: wrap}
+.selectmode .button{flex: 1;width:180px;height: 105px;background: url('/src/assets/icon/pay@2x.png') center no-repeat;background-size: 180px 105px;display:inline-block;flex-basis: 180px;display: flex;flex-direction: column;align-items: space-around}
+.selectmode .button.selected{background: url('/src/assets/icon/payselected@2x.png') center no-repeat;background-size: 180px 105px;}
+.selectmode .button .price{text-align: center;font-size: 18px;color: #222;display: block;margin-top: 21px}
+.selectmode .button .detail{text-align: center;font-size: 15px;color: #999;display: block;}
+.placehold{flex: 1;width:180px;height: 105px;display:inline-block;flex-basis: 180px;}
+ul,li{list-style: disc!important;list-style-position: inside!important;font-size: 15px;color:#494949;width: 88%;margin-left:6%}
+.handle{height:83px;width:100%;background-color: #fff;position: fixed;bottom:-2px;box-sizing: border-box;padding-top: 10px}
+.handle .button{width:84%;margin-left:8%;height:40px;line-height: 40px;border-radius: 20px;background-color: #06D5DE;text-align: center;color: #fff;font-size: 15px}
+</style>
---
+++ src/views/unsubscribe-view.vue
@@ -0,0 +1,35 @@
+<template>
+    <div style="width:100%;backgroundColor:#F8F8F8">
+        <div class="personinfo">
+            <group style="marginTop:12px">
+                <XInput title="姓名" v-model="name" required text-align="right"></XInput>
+                <XInput title="手机号" is-type="china-mobile" v-model="phone" required text-align="right" placeholder="请输入手机号" placeholder-align="right"></XInput>
+            </group>
+                <p>退订理由</p>
+            <group margin-top:>
+                <x-textarea title="" v-model="value" placeholder="请输入内容"></x-textarea>
+            </group>
+        </div>
+        <div class="handle">
+            <div class="button">提交</div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { Group , XInput , XTextarea } from 'vux'
+
+export default {
+    components:{
+        Group,
+        XInput,
+        XTextarea
+    }
+}
+</script>
+
+<style scoped>
+p{font-size: 16px;color:#222;font-weight: bold;margin-left: 15px;margin-top: 15px}
+.handle{height:83px;width:100%;background-color: #fff;position: fixed;bottom:0;box-sizing: border-box;padding-top: 10px}
+.handle .button{width:84%;margin-left:8%;height:40px;line-height: 40px;border-radius: 20px;background-color: #06D5DE;text-align: center;color: #fff;font-size: 15px}
+</style>
